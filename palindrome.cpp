5f55c2e7f9a6b126e366b92f13a265f7611cc2fb
miagrace06 <mcampanella@umail.ucsb.edu>
2024-11-19 07:59:30 -0800

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -8,23 +8,28 @@ using namespace std;
 #include "pheaders.h"
 
 int main() {
-    // 1. Get a string from the user (see examples given)
-    // 2. Clean up the string using the required function
-    // 3. Check to see if string is a palindrome
-    // 4. Print out message accordingly (see examples given)
+    // 1. declare a string variable to hold the user input
     string s;
-
+    // 2. Prompt the user to enter a sentence
     cout << "Enter sentence:";
+
+    //3. Read the entire line of input from the user, including spaces
     getline(cin, s);
+    // print a newline for formatting purposes
     cout << endl;
 
+    //4. Clean up the string by removing any unwanted characters
     cleanUp(s);
 
+    //5. check if the cleaned-up string is a palindrome
     if (isPalindrome(s)) {
+	    // 6. If it is a palindrome, print this message. 
 	    cout << "It is a palindrome." << endl;
     }else {
+	    //7. If it is not a palindrome, print this message.
 	    cout << "It is not a palindrome." << endl;
     }
+    //8. return 0 to indicate sufessful execution of the program
     return 0;
 }
 
